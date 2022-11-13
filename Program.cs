62695d3cd5e237e167e2857eea0fcaa6d94a08f4
Expired <imoses2@hotmail.com>
2022-11-13 16:35:53 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Minesweeper_Solver {
+	class Program {
+		static void Main(string[] args) {
+			// open minesweeper
+			
+			
+			// find the button ids
+			
+			
+			// convert to matrix
+			
+			
+			// reduce matrix
+			
+			
+			// click buttons
+			
+			
+			// repeat
+			
+			
+			
+		}
+	}
+}
