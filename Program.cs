771d2dec67a7467bc19e508d4f4dcf2a1de07f1e
Expired <imoses2@hotmail.com>
2022-11-19 23:40:03 -0700

    gets a screenshot and saves the pixels in an array

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ namespace Minesweeper_Solver {
 			{4, 3},
 			{4, 4}
 		};
+		static uint GHND = 0x0042;
 		
 		static void Main(string[] args) {
 			// open minesweeper
@@ -41,7 +42,7 @@ namespace Minesweeper_Solver {
 				location.Top  + FIRST_TOP  + (HEIGHT-1)*SPACING
 			);
 			Thread.Sleep(500);
-			SetCursorPos(location.Left, location.Top);
+			SetCursorPos(0, 0);
 			
 			//test
 			// SetCursorPos(location.Left + FIRST_LEFT + SPACING, location.Top + FIRST_TOP);
@@ -61,17 +62,108 @@ namespace Minesweeper_Solver {
 			// print(matrix);
 			
 			// get current game state
-			var watch = new System.Diagnostics.Stopwatch();
-			watch.Start();
+			// var watch = new System.Diagnostics.Stopwatch();
+			// watch.Start();
 			
-			int[,] gameState = new int[ HEIGHT, WIDTH ];
-			getGameState(location, gameState);
+			// int[,] gameState = new int[ HEIGHT, WIDTH ];
+			// getGameState(location, gameState);
 			
-			watch.Stop();
+			// watch.Stop();
 			
-			print(gameState);
-			Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+			// print(gameState);
+			// Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
 			
+			int elementSize = Marshal.SizeOf(typeof(IntPtr));
+			IntPtr unmanagedArray = Marshal.AllocHGlobal(10 * elementSize);
+			
+			for(int i=0; i<10; i++)
+				Console.WriteLine(Marshal.ReadIntPtr(unmanagedArray, i*elementSize));
+
+			for(int i=0; i<10; i++)
+				Marshal.WriteIntPtr(unmanagedArray, i*elementSize, ((IntPtr)(i+1)));
+			
+			for(int i=0; i<10; i++)
+				Console.WriteLine(Marshal.ReadIntPtr(unmanagedArray, i*elementSize));
+			
+			Marshal.FreeHGlobal(unmanagedArray);
+
+			unsafe {
+				int w = location.Right - location.Left;
+				int h = location.Bottom - location.Top;
+				IntPtr hdcScreen = GetDesktopWindow();
+				IntPtr hdcWindow = GetWindowDC(hdcScreen);
+				IntPtr hdcMemDC = CreateCompatibleDC(hdcWindow);
+				Rect rcClient = location;
+				IntPtr hbmScreen = CreateCompatibleBitmap(hdcWindow, rcClient.Right-rcClient.Left, rcClient.Bottom-rcClient.Top);
+
+
+				SelectObject(hdcMemDC, hbmScreen);
+				
+				bool result = BitBlt(
+					// to
+					hdcMemDC, 0, 0, rcClient.Right-rcClient.Left, rcClient.Bottom-rcClient.Top,
+					// from
+					hdcWindow, location.Left, location.Top,
+					// command
+					TernaryRasterOperations.SRCCOPY
+				);
+				Console.WriteLine(result.ToString());
+
+				BITMAP bmpScreen = new BITMAP();
+				GCHandle hndl = GCHandle.Alloc(bmpScreen, GCHandleType.Pinned);
+				IntPtr ptrToBitmap = hndl.AddrOfPinnedObject();
+				int result2 = GetObject(hbmScreen, Marshal.SizeOf<BITMAP>(), ptrToBitmap);
+				bmpScreen = Marshal.PtrToStructure<BITMAP>(ptrToBitmap);
+				hndl.Free();
+				
+				Console.WriteLine($"GetObject: {result2}");
+
+				BITMAPINFOHEADER bi = new BITMAPINFOHEADER();
+				bi.biSize = (uint) sizeof(BITMAPINFOHEADER);
+				bi.biWidth = bmpScreen.bmWidth;
+				bi.biHeight = bmpScreen.bmHeight;
+				bi.biPlanes = 1;
+				bi.biBitCount = 32;
+				bi.biCompression = BitmapCompressionMode.BI_RGB;
+				bi.biSizeImage = 0;
+				bi.biClrUsed = 0;
+				bi.biClrImportant = 0;
+				
+				int dwBmpSize = ((bmpScreen.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmpScreen.bmHeight;
+				// IntPtr hDIB = GlobalAlloc(GHND, dwBmpSize);
+				byte[] lpbitmap = new byte[dwBmpSize];
+				
+				BITMAPINFO bmi = new BITMAPINFO();
+				bmi.bmiHeader = bi;
+
+				int result3 = GetDIBits(
+					hdcWindow, hbmScreen, 0, 
+					(uint) h,
+					lpbitmap, 
+					ref bmi, DIB_Color_Mode.DIB_RGB_COLORS
+				);
+				
+				Console.WriteLine($"GetDIBits: {result3}");
+				
+				Color c = GetColorAt(hdcWindow, location.Left, location.Top);
+				Console.WriteLine(c.ToString());
+				
+				int _row = 200;
+				int i=0;
+				while(i<w*4) {
+					Console.WriteLine($"{lpbitmap[_row*w*4+i]} {lpbitmap[_row*w*4+i+1]} {lpbitmap[_row*w*4+i+2]} {lpbitmap[_row*w*4+i+3]}");
+					i+=4;
+				}
+				i-=4;
+				// Console.WriteLine($"{lpbitmap[i]} {lpbitmap[i+1]} {lpbitmap[i+2]}");
+				Console.WriteLine(i/4);
+				
+				//clean
+				DeleteObject(hbmScreen);
+				DeleteObject(hdcMemDC);
+				ReleaseDC(hdcWindow, hdcScreen);
+				
+			}
 			
 			
 			
@@ -93,6 +185,181 @@ namespace Minesweeper_Solver {
 			public int Top { get; set; }
 			public int Right { get; set; }
 			public int Bottom { get; set; }
+		}
+
+		enum TernaryRasterOperations : uint {
+				/// <summary>dest = source</summary>
+				SRCCOPY = 0x00CC0020,
+				/// <summary>dest = source OR dest</summary>
+				SRCPAINT = 0x00EE0086,
+				/// <summary>dest = source AND dest</summary>
+				SRCAND = 0x008800C6,
+				/// <summary>dest = source XOR dest</summary>
+				SRCINVERT = 0x00660046,
+				/// <summary>dest = source AND (NOT dest)</summary>
+				SRCERASE = 0x00440328,
+				/// <summary>dest = (NOT source)</summary>
+				NOTSRCCOPY = 0x00330008,
+				/// <summary>dest = (NOT src) AND (NOT dest)</summary>
+				NOTSRCERASE = 0x001100A6,
+				/// <summary>dest = (source AND pattern)</summary>
+				MERGECOPY = 0x00C000CA,
+				/// <summary>dest = (NOT source) OR dest</summary>
+				MERGEPAINT = 0x00BB0226,
+				/// <summary>dest = pattern</summary>
+				PATCOPY    = 0x00F00021,
+				/// <summary>dest = DPSnoo</summary>
+				PATPAINT = 0x00FB0A09,
+				/// <summary>dest = pattern XOR dest</summary>
+				PATINVERT = 0x005A0049,
+				/// <summary>dest = (NOT dest)</summary>
+				DSTINVERT = 0x00550009,
+				/// <summary>dest = BLACK</summary>
+				BLACKNESS = 0x00000042,
+				/// <summary>dest = WHITE</summary>
+				WHITENESS = 0x00FF0062,
+				/// <summary>
+				/// Capture window as seen on screen.  This includes layered windows
+				/// such as WPF windows with AllowsTransparency="true"
+				/// </summary>
+				CAPTUREBLT = 0x40000000
+		}
+
+		[Serializable]
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Bitmap {
+				 /// <summary>
+				 /// The bitmap type. This member must be zero.
+				 /// </summary>
+				 public int bmType;
+
+				 /// <summary>
+				 /// The width, in pixels, of the bitmap. The width must be greater than zero.
+				 /// </summary>
+				 public int bmWidth;
+
+				 /// <summary>
+				 /// The height, in pixels, of the bitmap. The height must be greater than zero.
+				 /// </summary>
+				 public int bmHeight;
+
+				 /// <summary>
+				 /// The number of bytes in each scan line. This value must be divisible by 2, because the system assumes that the bit
+				 /// values of a bitmap form an array that is word aligned.
+				 /// </summary>
+				 public int bmWidthBytes;
+
+				 /// <summary>
+				 /// The count of color planes.
+				 /// </summary>
+				 public int bmPlanes;
+
+				 /// <summary>
+				 /// The number of bits required to indicate the color of a pixel.
+				 /// </summary>
+				 public int bmBitsPixel;
+
+				 /// <summary>
+				 /// A pointer to the location of the bit values for the bitmap. The bmBits member must be a pointer to an array of
+				 /// character (1-byte) values.
+				 /// </summary>
+				 public IntPtr bmBits;
+		}
+
+		[StructLayoutAttribute( LayoutKind.Sequential )]
+		struct BITMAPINFO {
+			 /// <summary>
+			 /// A BITMAPINFOHEADER structure that contains information about the dimensions of color format.
+			 /// </summary>
+			 public BITMAPINFOHEADER bmiHeader;
+
+			 /// <summary>
+			 /// An array of RGBQUAD. The elements of the array that make up the color table.
+			 /// </summary>
+			 [MarshalAsAttribute( UnmanagedType.ByValArray, SizeConst = 1, ArraySubType = UnmanagedType.Struct )]
+			 public RGBQUAD[] bmiColors;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		struct BITMAPINFOHEADER {
+				public uint  biSize;
+				public int   biWidth;
+				public int   biHeight;
+				public ushort   biPlanes;
+				public ushort   biBitCount;
+				public BitmapCompressionMode  biCompression;
+				public uint  biSizeImage;
+				public int   biXPelsPerMeter;
+				public int   biYPelsPerMeter;
+				public uint  biClrUsed;
+				public uint  biClrImportant;
+
+				public void Init() {
+						biSize = (uint)Marshal.SizeOf(this);
+				}
+		}
+
+		enum BitmapCompressionMode : uint {
+			 BI_RGB = 0,
+			 BI_RLE8 = 1,
+			 BI_RLE4 = 2,
+			 BI_BITFIELDS = 3,
+			 BI_JPEG = 4,
+			 BI_PNG = 5
+		}
+
+		[StructLayout(LayoutKind.Sequential, Pack=1)]
+		public struct RGBQUAD {
+				public byte rgbBlue;
+				public byte rgbGreen;
+				public byte rgbRed;
+				public byte rgbReserved;
+		}
+
+		enum DIB_Color_Mode : uint {
+			 DIB_RGB_COLORS = 0,
+			 DIB_PAL_COLORS = 1
+		}
+
+		[Serializable]
+		[StructLayout(LayoutKind.Sequential)]
+		public struct BITMAP {
+				 /// <summary>
+				 /// The bitmap type. This member must be zero.
+				 /// </summary>
+				 public int bmType;
+
+				 /// <summary>
+				 /// The width, in pixels, of the bitmap. The width must be greater than zero.
+				 /// </summary>
+				 public int bmWidth;
+
+				 /// <summary>
+				 /// The height, in pixels, of the bitmap. The height must be greater than zero.
+				 /// </summary>
+				 public int bmHeight;
+
+				 /// <summary>
+				 /// The number of bytes in each scan line. This value must be divisible by 2, because the system assumes that the bit
+				 /// values of a bitmap form an array that is word aligned.
+				 /// </summary>
+				 public int bmWidthBytes;
+
+				 /// <summary>
+				 /// The count of color planes.
+				 /// </summary>
+				 public int bmPlanes;
+
+				 /// <summary>
+				 /// The number of bits required to indicate the color of a pixel.
+				 /// </summary>
+				 public int bmBitsPixel;
+
+				 /// <summary>
+				 /// A pointer to the location of the bit values for the bitmap. The bmBits member must be a pointer to an array of
+				 /// character (1-byte) values.
+				 /// </summary>
+				 public IntPtr bmBits;
 		}
 
 		// METHODS
@@ -284,5 +551,26 @@ namespace Minesweeper_Solver {
 		public static extern uint GetPixel(IntPtr dc, int x, int y);
 		[DllImport("user32.dll", SetLastError = true)]
 		public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+		
+		[DllImport("gdi32.dll", EntryPoint = "BitBlt", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool BitBlt([In] IntPtr hdc, int nXDest, int nYDest, int nWidth, int nHeight, [In] IntPtr hdcSrc, int nXSrc, int nYSrc, TernaryRasterOperations dwRop);
+		
+		[DllImport("gdi32.dll", EntryPoint = "CreateCompatibleDC", SetLastError=true)]
+		static extern IntPtr CreateCompatibleDC([In] IntPtr hdc);
+		[DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap")]
+		static extern IntPtr CreateCompatibleBitmap([In] IntPtr hdc, int nWidth, int nHeight);
+		[DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+		public static extern IntPtr SelectObject([In] IntPtr hdc, [In] IntPtr hgdiobj);
+		[DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool DeleteObject([In] IntPtr hObject);
+		[DllImport("gdi32.dll", EntryPoint = "GetDIBits")]
+		static extern int GetDIBits([In] IntPtr hdc, [In] IntPtr hbmp, uint uStartScan, uint cScanLines, [Out] byte[] lpvBits, ref BITMAPINFO lpbi, DIB_Color_Mode uUsage);
+		[DllImport("gdi32.dll")]
+		static extern int GetObject(IntPtr hgdiobj, int cbBuffer, IntPtr lpvObject);
+		
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GlobalAlloc(uint uFlags, UIntPtr dwBytes);
 	}	
 }
