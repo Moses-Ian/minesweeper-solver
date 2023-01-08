b59776523735b39b868529fdc9e96188ba1f887a
Ian Moses <imoses2@hotmail.com>
2023-01-08 01:12:30 -0700

    Updates

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ window.onload = () => {
 	const isBot = urlParams.get('bot');
 
 	if (isBot) {
-		console.log("this is a bot");
 		
 		const warningElement = document.getElementById("warning");
-		console.log(warningElement);
 		warningElement.style.display = 'block';
 		
 		const blinkElement = document.getElementById("blink");
 		setInterval(() => blink.style.opacity = 1 - blink.style.opacity, 1000);
+		
+		const downloadElement = document.getElementById("download");
+		downloadElement.style.display = 'none';
+		
 	}
 }
